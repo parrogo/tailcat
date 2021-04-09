ddacd1c18718908fcc7b28099314e5cb6b8954c6
Andrea Parodi <andrea@parro.it>
2021-04-09 16:58:18 +0200

    Update tailcat.go

--- tailcat.go
+++ tailcat.go
@@ -3,9 +3,9 @@ package tailcat
 
 // # TODO: remove or rename `Func` function
 
-/* # TODO: configure issues & PR in Code Climate
-# You can configure settings [here](https://codeclimate.com/repos/607069cb64111c400c006c70/edit)
-*/
+// # TODO: configure issues & PR in Code Climate
+// # You can configure settings [here](https://codeclimate.com/repos/607069cb64111c400c006c70/edit)
+
 
 // Func answers
 func Func() int {
